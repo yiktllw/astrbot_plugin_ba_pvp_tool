c62b53560110be8b67c90b836e9bbf20b6239f63
yiktllw <yiktllw@qq.com>
2025-09-21 16:00:45 +0800

    feat: init

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+class BA_PVP_Tool(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
-    async def initialize(self):
+    async def initialize(self, context: Context, config: AstrBotConfig):
+        super().__init__(context)
+        self.notice_id = config.get("notice_id", "")
+        self.server = config.get("server", "TW")
+        self.friend_code = config.get("friend_code", "")
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
+ 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
     async def helloworld(self, event: AstrMessageEvent):
